50b1672fa2f0bdd7390d4ba37cbaa435f837132c
井上翼 <tsubasa0129ti@gmail.com>
2023-10-10 20:57:32 +0900

    feat: v0 初期画面の描画の実行を可能とした

---
+++ src/test/java/com/example/Compass/CompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Compass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
